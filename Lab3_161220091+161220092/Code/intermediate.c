782eab4063550126b92cbf93a435dbddee0b2534
HuaMeng <dreamanne15@gmail.com>
2019-05-13 23:51:39 +0800

    Add basic list operation.

--- Lab3_161220091+161220092/Code/intermediate.c
+++ Lab3_161220091+161220092/Code/intermediate.c
@@ -1,5 +1,58 @@
 #include "intermediate.h"
 
+Operand GenerateOperand(int kind, int value) {
+	Operand op = (Operand)malloc(sizeof(struct Operand_));
+	switch(kind) {
+		case 0: op->kind = VARIABLE; op->u.var_no = value; break;
+		case 1: op->kind = CONSTANT; op->u.value = value; break;
+ 		case 2: op->kind = ADDRESS; op->u.value = value; break;
+		default: printf("Error kind in GenerateOperand\n");
+	}
+	return op;
+}
+
+InterCode GenerateInterCodeAssign(Operand right, Operand left) {
+	InterCode ic = (InterCode)malloc(sizeof(struct InterCode_));
+	ic->kind = ASSIGN;
+	ic->u.assign.right = right;
+	ic->u.assign.left = left;
+	return ic;
+}
+
+InterCode GenerateInterCodeOp(Operand result, Operand op1, Operand op2, int op) {
+	InterCode ic = (InterCode)malloc(sizeof(struct InterCode_));
+	ic->kind = op;
+	ic->u.binop.result = result;
+	ic->u.binop.op1 = op1;
+	ic->u.binop.op2 = op2;
+	return ic;
+}
+
+void InitialInterCodes() {
+	head = (InterCodes)malloc(sizeof(struct InterCodes_));
+	tail = (InterCodes)malloc(sizeof(struct InterCodes_));
+	head->pre = tail->next = NULL;
+	head->next = tail;
+	tail->pre = head;
+}
+
+int InsertInterCodes(InterCode insert) {
+	InterCodes ics = (InterCodes)malloc(sizeof(struct InterCodes_));
+	ics->code = insert;
+	ics->pre = tail->pre;
+	ics->next = tail;
+	tail->pre->next = ics;
+	tail->pre = ics;
+	return 1;
+}
+
+int DeleteInterCodes(InterCodes del) {
+	del->pre->next = del->next;
+	del->next->pre = del->pre;
+	free(del);
+	return 1;
+}
+
 void writeToFile(FILE *f) {
 	return;
 }
--- Lab3_161220091+161220092/Code/intermediate.h
+++ Lab3_161220091+161220092/Code/intermediate.h
@@ -1,3 +1,45 @@
 #include "semantic.h"
 
+typedef struct Operand_* Operand;
+typedef struct InterCodes_* InterCodes;
+typedef struct InterCode_* InterCode;
+
+struct Operand_ {
+	enum { VARIABLE, CONSTANT, ADDRESS } kind;
+	union {
+		int var_no;
+		int value;
+	} u;
+};
+
+struct InterCode_ {
+	enum { ADD, SUB, MUL, DIV, ASSIGN} kind;
+	union {
+		struct { Operand result, op1, op2; } binop;
+		struct { Operand right, left; } assign;
+	} u;
+};
+
+struct InterCodes_ {
+	InterCode code;
+	InterCodes pre, next;
+};
+
+InterCodes head;
+InterCodes tail;
+
+/* Operation of Operand */
+// kind { variable:0 constant:1 address:2 } */
+Operand GenerateOperand(int kind, int value);
+
+/* Operation of InterCode */
+InterCode GenerateInterCodeAssign(Operand right, Operand left);
+// op { + is 0, - is 1, * is 2, / is 3 }
+InterCode GenerateInterCodeOp(Operand result, Operand op1, Operand op2, int op);
+
+/* Operation of InterCodes */
+void InitialInterCodes();
+int InsertInterCodes(InterCode insert);
+int DeleteInterCodes(InterCodes del);
+
 void writeToFile(FILE *f);
